4b2d994a2c172df6d596759c28ccfd644f14fc6b
datuna2011 <datunamamulashvili4@gmail.com>
2024-08-28 20:29:37 +0400

    commit

---
+++ level 07/homework/homework.py
@@ -0,0 +1,27 @@
+# 2) დავალება
+
+# 1) მომხმარებელს შემოატანინეთ 4 სხავდასხვა რიცხვი და ამ ოთხივე რიცხვზე მოახდინეთ არითმეტიკური ოპერაციები. +,-,*,//,**
+# 2) დაწერეთ პროგრამა სადაც შემოიტანთ თქვენი ოჯახის წევრების ასაკს და გამოიტანეთ ის თუ რამდენი წლის იქნებიან 20 წლის შემდეგ.
+# 3) შექმენით ცვლადები სადაც შეინახავთ თქვენს თავზე ინფორმაციას (სახელი, გვარი, ასაკი, ქვეყანა, ქალაქი, საყვარელი ფერი, საყვარელი მანქანა, საყვარელი საჭმელი, საყვარელი სპორტი და ა.შ) შემდეგ შემოტანილი მნიშვნელობებით ააწყვეტ ერთი დიდი წინადადება. 
+
+
+print(type("daviti"))
+print(type(7))
+print("daviti"+1)
+
+
+input_number1=int(input("enter number> "))
+input_number2=int(input("enter number> "))
+input_number3=int(input("enter number> "))
+input_number4=int(input("enter number> "))
+print(input_number1+input_number2)
+print(input_number2-input_number1)
+print(input_number3*input_number4)
+print(input_number4-input_number1)
+print(input_number3+input_number4)
+print(input_number2-input_number1)
+print(input_number3*input_number2)
+
+
+
+
