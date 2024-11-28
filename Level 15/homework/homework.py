5c97064f7b38be983feaa9034018700c774ee722
datuna2011 <datunamamulashvili4@gmail.com>
2024-11-29 00:37:59 +0400

    commit

---
+++ Level 15/homework/homework.py
@@ -0,0 +1,21 @@
+# 1) 
+# აბსოლიტურად ყველა განვლილი მასალა გაიმეორეთ, გააკეთეთ თითო საკითხზე 10მაგალითი.
+
+# გაიმეორეთ, input, logical operator, comparison operator
+
+
+# ეს ჩამოთვლილი საკითხები გაიმეორეთ + sololearn თავიდან ბოლომდე გააკეთეთ, module 1 დან დაიწყეთ. 
+
+# name = int (input("Enter any name") )
+# print("", name)
+
+
+# name = str (input("Enter your name"))
+# print("sheni saxeli aris", name)
+
+# name = input("Enter your name")
+# print(name,13)
+
+# last_name = input("enter your last_name")
+# print(last_name,"")
+
