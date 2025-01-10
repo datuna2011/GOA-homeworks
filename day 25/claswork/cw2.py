36fb1997f720b50c2f7d8f3fdf3de92e154d7d2b
datuna2011 <datunamamulashvili4@gmail.com>
2025-01-10 17:43:10 +0400

    commit

---
+++ day 25/claswork/cw2.py
@@ -0,0 +1,4 @@
+queue = ['John', 'Amy', 'Bob', 'Adam']
+queue2 = input("დაამატე სახელი")
+queue.append(queue2)
+print(queue)
---
+++ day 25/claswork/cw3.py
@@ -0,0 +1,8 @@
+nums = [2,4,8,9,5]
+nums.insert (1, 3)
+nums.remove(9)
+nums.isert(0, nums.count (8))
+print(nums[3])
+
+
+# კოდში მოიპოვება შეცდომა, რადგან
